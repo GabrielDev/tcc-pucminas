89c616fc4fa9eddf868951ac9aba12afb89fe95b
GabrielDev <gabriel.dos.santos.lucena@gmail.com>
2020-04-06 17:58:06 -0300

    Ajustes de deploy

--- doggis-ui/src/app/pages/avaliacao/avaliacao.module.ts
+++ doggis-ui/src/app/pages/avaliacao/avaliacao.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { AvaliacaoComponent } from './avaliacao.component';
 
 @NgModule({
   declarations: [AvaliacaoComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
   ]
 })
 export class AvaliacaoModule { }
