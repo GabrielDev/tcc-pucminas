de7834ae0e9febb7614914e6026dcc9ef1d00caa
Ubuntu <ubuntu@ip-172-31-3-218.us-west-2.compute.internal>
2020-04-14 22:37:26 +0000

    Corrigindo script para o ubuntu

--- populateMySqlDB.sh
+++ populateMySqlDB.sh
@@ -6,7 +6,7 @@ dbRunning=`docker ps --format '{{.Names}}' | grep doggisApp-MYSQL | wc -l`
 
 if [ $dbRunning == "1" ]; then
   if [ $running != "1" ]; then
-    docker run -d --rm --name mysqlscript --link doggisApp-MYSQL:mysql -e MYSQL_RANDOM_ROOT_PASSWORD=true -v $(PWD):/tmp/scripts mysql:5.7.29
+    docker run -d --rm --name mysqlscript --link doggisApp-MYSQL:mysql -e MYSQL_RANDOM_ROOT_PASSWORD=true -v $PWD:/tmp/scripts mysql:5.7.29
     docker exec -it mysqlscript sh -c 'mysql -h doggisApp-MYSQL -P 3306 -u root -pDoggis@2020 < /tmp/scripts/data.sql'
     docker stop mysqlscript
   else
--- startMySqlDB.sh
+++ startMySqlDB.sh
@@ -12,5 +12,5 @@ running=`docker ps --format '{{.Names}}' | grep doggisApp-MYSQL | wc -l`
 if [ $running == "1" ]; then
   echo "Container already started"
 else
-    docker run --rm -p 3306:3306 --name doggisApp-MYSQL -v $(PWD)/.mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=Doggis@2020 -e MYSQL_DATABASE=doggis -e MYSQL_USER=doggisApp -e MYSQL_PASSWORD=Doggis@2019 -d  mysql:5.7.29
+    docker run --rm -p 3306:3306 --name doggisApp-MYSQL -v $PWD/.mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=Doggis@2020 -e MYSQL_DATABASE=doggis -e MYSQL_USER=doggisApp -e MYSQL_PASSWORD=Doggis@2019 -d  mysql:5.7.29
 fi
