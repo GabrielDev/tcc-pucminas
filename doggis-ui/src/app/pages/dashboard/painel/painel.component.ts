b7031866d840c2f68587cf36c047921246f81c02
GabrielDev <gabriel.dos.santos.lucena@gmail.com>
2020-05-22 18:02:50 -0300

    Ajustes de dependencias

--- doggis-ui/src/app/pages/dashboard/painel/painel.component.ts
+++ doggis-ui/src/app/pages/dashboard/painel/painel.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js';
 import { parseOptions, chartOptions, chartExample2, chartExample1 } from 'src/app/variables/charts';
+
 
 @Component({
   selector: 'app-painel',
